d8f66dd43cd40b4908980073c898f04149e68093
bitbrain-za <phil@bitbrain.co.za>
2018-03-25 16:57:37 +0200

    test

--- mqtt_service/secrets.cs
+++ mqtt_service/secrets.cs
@@ -8,7 +8,7 @@ namespace mqtt_service
 {
 	internal static class secrets
 	{
-		internal const string ip = "192.168.1.1";
+		internal const string ip = "192.168.1.32";
 		internal const int port = 1883;
 		internal const string username = "user";
 		internal const string password = "password";
