692e0453b6e87c4131a03747125140d3120f210b
bitbrain-za <phil@bitbrain.co.za>
2018-03-25 17:01:30 +0200

    ignore test

--- mqtt_service/secrets.cs
+++ mqtt_service/secrets.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 
 namespace mqtt_service
 {
+	/*
 	internal static class secrets
 	{
-		internal const string ip = "192.168.1.32";
+		internal const string ip = "192.168.1.1";
 		internal const int port = 1883;
 		internal const string username = "user";
 		internal const string password = "password";
 		internal const string topic_test = "home-assistant/test";
 		internal const string topic_desktop = "home-assistant/pc";
 	}
+	*/
 }
